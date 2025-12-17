29180f865fe613fbb62033a2d9f13898c42d90e3
Hassan <dhhacby@gmail.com>
2025-12-18 01:02:18 +0500

    Connection of server side established

--- Secure-File-Transfer-and-Integrity-Verification-System/Server.py
+++ Secure-File-Transfer-and-Integrity-Verification-System/Server.py
@@ -3,3 +3,11 @@ import re
 import hashlib
 import shutil
 import os
+Host = "192.168.1.1"
+Port = 5000
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(Host,Port)
+server.listen(1)
+print("Server is waiting for Client...")
+conn , adds = server.accept()
+print("connected by", adds)
