4eb335eb4955ad048e7b01cdf38f26ad34eb0902
Hassan <dhhacby@gmail.com>
2025-12-18 14:38:40 +0500

    writing file data at server side

--- Secure-File-Transfer-and-Integrity-Verification-System/Server.py
+++ Secure-File-Transfer-and-Integrity-Verification-System/Server.py
@@ -6,8 +6,8 @@ import os
 Host = "127.0.0.1"
 Port = 5000
 
-
-
+os.makedirs("incomingFiles",exist_ok=True)
+log_file_path = os.path.join("incomingFiles", "log_file.txt")
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((Host,Port))
 server.listen(1)
@@ -18,3 +18,12 @@ metadata = conn.recv(1024).decode()
 filename,filesize,client_hash = metadata.split("|")
 filesize = int(filesize)
 conn.send(b"ok")
+received = 0
+with open(log_file_path,"wb") as f:
+    while received < filesize:
+        data = conn.recv(4096)
+        if not data:
+            break
+        f.write(data)
+        received += len(data)
+
