8f8586e7498ddfc3fd9f0d8ec84c8d5137a2ad6d
Hassan <dhhacby@gmail.com>
2025-12-18 14:51:46 +0500

    verifying hashing

--- Secure-File-Transfer-and-Integrity-Verification-System/Server.py
+++ Secure-File-Transfer-and-Integrity-Verification-System/Server.py
@@ -6,8 +6,13 @@ import os
 Host = "127.0.0.1"
 Port = 5000
 
-os.makedirs("incomingFiles",exist_ok=True)
-log_file_path = os.path.join("incomingFiles", "log_file.txt")
+def hash_file(path):
+    sha = hashlib.sha256()
+    with open(path,"rb") as f:
+        while chunk := f.read(4096):
+            sha.update(chunk)
+        return sha.hexdigest()
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((Host,Port))
 server.listen(1)
@@ -18,6 +23,9 @@ metadata = conn.recv(1024).decode()
 filename,filesize,client_hash = metadata.split("|")
 filesize = int(filesize)
 conn.send(b"ok")
+
+os.makedirs("incomingFiles",exist_ok=True)
+log_file_path = os.path.join("incomingFiles", "log_file.txt")
 received = 0
 with open(log_file_path,"wb") as f:
     while received < filesize:
@@ -26,4 +34,12 @@ with open(log_file_path,"wb") as f:
             break
         f.write(data)
         received += len(data)
+
+
+        #verify hash
+server_hash = hash_file(log_file_path)
+if server_hash == client_hash:
+    print("File integrity verified")
+else:
+    print("Warning: Hash mismatched")
 
