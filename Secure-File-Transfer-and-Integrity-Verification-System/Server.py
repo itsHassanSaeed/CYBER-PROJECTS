9a0867928cc2c08fee3ecf8ed7b757cb8ed235fc
Hassan <dhhacby@gmail.com>
2025-12-21 18:44:30 +0500

    threading for multiple connections

--- Secure-File-Transfer-and-Integrity-Verification-System/Server.py
+++ Secure-File-Transfer-and-Integrity-Verification-System/Server.py
@@ -4,6 +4,7 @@ import hashlib
 import shutil
 import os
 import subprocess
+import threading
 Host = "127.0.0.1"
 Port = 5000
 
@@ -14,49 +15,59 @@ def hash_file(path):
             sha.update(chunk)
         return sha.hexdigest()
 
+
+
+
+
+def handle_client(conn,adds):
+    print(f"[NEW CONNECTION] {adds} connected")
+    metadata = conn.recv(1024).decode()
+    filename,filesize,client_hash = metadata.split("|")
+    filesize = int(filesize)
+    if not re.fullmatch(r"[a-zA-Z0-9_]+\.(txt|log)", filename):
+        conn.send(b"Invalid filename detected.")
+        conn.close()
+        server.close()
+        exit()
+    conn.send(b"ok")
+
+    os.makedirs("incomingFiles",exist_ok=True)
+    filesaved = filename
+    log_file_path = os.path.join("incomingFiles", filesaved)
+    received = 0
+    with open(log_file_path,"wb") as f:
+        while received < filesize:
+            data = conn.recv(4096)
+            if not data:
+                break
+            f.write(data)
+            received += len(data)
+
+
+        
+    #file backup
+    backup_file_path = os.path.join("incomingFiles", "backup_file.txt")
+    shutil.copy2(log_file_path,backup_file_path)
+
+    # Run OS command (file details)
+    subprocess.run(["dir" if os.name == "nt" else "ls", "incomingFiles"], shell=True)
+
+        #verify hash
+    server_hash = hash_file(log_file_path)
+    if server_hash == client_hash:
+        print("File integrity verified")
+    else:
+        print("Warning: Hash mismatched")
+
+
+    conn.close()
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((Host,Port))
 server.listen(1)
 print("Server is waiting for Client...")
-conn , adds = server.accept()
-print("connected by", adds)
-metadata = conn.recv(1024).decode()
-filename,filesize,client_hash = metadata.split("|")
-filesize = int(filesize)
-if not re.fullmatch(r"[a-zA-Z0-9_]+\.(txt|log)", filename):
-    conn.send(b"Invalid filename detected.")
-    conn.close()
-    server.close()
-    exit()
-conn.send(b"ok")
 
-os.makedirs("incomingFiles",exist_ok=True)
-log_file_path = os.path.join("incomingFiles", "log_file.txt")
-received = 0
-with open(log_file_path,"wb") as f:
-    while received < filesize:
-        data = conn.recv(4096)
-        if not data:
-            break
-        f.write(data)
-        received += len(data)
-
-
-    
-#file backup
-backup_file_path = os.path.join("incomingFiles", "backup_file.txt")
-shutil.copy2(log_file_path,backup_file_path)
-
-# Run OS command (file details)
-subprocess.run(["dir" if os.name == "nt" else "ls", "incomingFiles"], shell=True)
-
-    #verify hash
-server_hash = hash_file(log_file_path)
-if server_hash == client_hash:
-    print("File integrity verified")
-else:
-    print("Warning: Hash mismatched")
-
-
-conn.close()
-server.close()
+while True:
+    conn, adds = server.accept()
+    thread = threading.Thread(target=handle_client, args=(conn, adds))
+    thread.start()
+    print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
